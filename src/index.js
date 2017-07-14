5d9721ffadeb5e42a16ff3702a5742d531defb5f
Zhuyun DAI <dzy197810@hotmail.com>
2017-07-14 16:46:47 -0400

    Change the comment of the index.js file.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* for debug only */
+/* this file is only used for debug purpose */
 
 var func = require('../src/longest-absolute-file-path');
 var result = func('dir\n\t        file.txt\n\tfile2.txt');
