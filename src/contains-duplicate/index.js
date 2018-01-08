afd1d2c0663e18e55168e416c55775ea7e6337f8
Zhuyun DAI <dzy197810@hotmail.com>
2018-01-07 22:25:48 -0500

    Commit contains duplicate

---
+++ src/contains-duplicate/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @return {boolean}
+ */
+const containsDuplicate = function (numbers) {
+    let set = new Set();
+    for (let n of numbers) {
+        if (set.has(n)) {
+            return true;
+        }
+        set.add(n);
+    }
+    return false;
+};
