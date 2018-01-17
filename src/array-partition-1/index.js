910096f0a40d737f52f23debb3118dd547018092
Zhuyun DAI <dzy197810@hotmail.com>
2018-01-16 22:49:53 -0500

    Commit solution for array partition I

---
+++ src/array-partition-1/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const arrayPairSum = function (nums) {
+    nums.sort((a, b) => a - b);
+    let sum = 0;
+    for (let i = 0; i < nums.length; i += 2) {
+        sum += nums[i];
+    }
+    return sum;
+};
+
+module.exports = arrayPairSum;
