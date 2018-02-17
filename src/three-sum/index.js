ce826900b62f284769873ecec5b1a31e693b971e
Zhuyun DAI <dzy197810@hotmail.com>
2018-02-16 23:01:47 -0500

    Commit new solution for three sum

--- src/three-sum/index.js
+++ src/three-sum/index.js
@@ -2,7 +2,7 @@
  * @param {number[]} numbers
  * @return {number[][]}
  */
-const threeSum = function (numbers) {
+const threeSumByMap = function (numbers) {
     let results = [];
     if (!numbers || numbers.length < 3) {
         return results;
@@ -36,5 +36,40 @@ const threeSum = function (numbers) {
     }
     return results;
 };
+
+const threeSum = function (nums) {
+    nums.sort((a, b) => a - b);
+    let ret = [];
+    let prevNum = NaN;
+    for (let i0 = 0; i0 < nums.length; ++i0) {
+        if (nums[i0] === prevNum) {
+            continue;   // skip duplication
+        }
+        let i1 = i0 + 1, i2 = nums.length - 1;
+        while (i1 < i2) {
+            let sum = nums[i0] + nums[i1] + nums[i2];
+            if (sum > 0) {
+                i2 = findDistinctNumberIndex(nums, i2, -1);
+            } else if (sum < 0) {
+                i1 = findDistinctNumberIndex(nums, i1, 1);
+            } else {
+                // sum === 0
+                ret.push([nums[i0], nums[i1], nums[i2]]);
+                i1 = findDistinctNumberIndex(nums, i1, 1);
+                i2 = findDistinctNumberIndex(nums, i2, -1);
+            }
+        }
+        prevNum = nums[i0];
+    }
+    return ret;
+};
+
+function findDistinctNumberIndex(nums, start, direction) {
+    let idx = start + direction;
+    while (idx >= 0 && nums[idx] === nums[start]) {
+        idx += direction;
+    }
+    return idx;
+}
 
 module.exports = threeSum;
