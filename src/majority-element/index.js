ff3c61888a47d49f526eefdccc997de75eee5e6c
Zhuyun DAI <dzy197810@hotmail.com>
2018-03-01 17:39:25 -0500

    Change to use bit manipulation for majority element

--- src/majority-element/index.js
+++ src/majority-element/index.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-const majorityElement = function (nums) {
+const majorityElementByMap = function (nums) {
     const threshold = Math.floor(nums.length / 2);
     const map = new Map();
     for (let i = 0; i < nums.length; ++i) {
@@ -15,6 +15,26 @@ const majorityElement = function (nums) {
         map.set(num, cnt);
     }
     throw "no solution";
+};
+
+const majorityElement = function (nums) {
+    let bitOnes = new Array(32);
+    bitOnes.fill(0);
+    for (let num of nums) {
+        for (let i = 0; i < bitOnes.length; ++i) {
+            let mask = 1 << i;
+            if (num & mask) {
+                ++bitOnes[i];
+            }
+        }
+    }
+    let result = 0, half = nums.length >> 1;
+    for (let i = 0; i < bitOnes.length; ++i) {
+        if (bitOnes[i] > half) {
+            result |= (1 << i);
+        }
+    }
+    return result;
 };
 
 module.exports = majorityElement;
